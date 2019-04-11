403fec7ae67a54c1136936295aa71991208c7175
David Kalen <kalen@my.uri.edu>
2019-04-10 22:06:27 -0400

    also match on prep

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,8 @@
   <div>
     <input v-model="keywords" placeholder="search">
     <div class="recipes-list">
-      <masonry
-        :cols="{default: 6,2880: 5,2400: 4,1920: 3, 1440: 2, 960: 1 }"
-        :gutter="30"
-      >
-      <recipe v-for="recipe in results" :recipe="recipe" :key="recipe.name"></recipe>
+      <masonry :cols="{default: 6,2880: 5,2400: 4,1920: 3, 1440: 2, 960: 1 }" :gutter="30">
+        <recipe v-for="recipe in results" :recipe="recipe" :key="recipe.name"></recipe>
       </masonry>
     </div>
   </div>
@@ -18,22 +15,36 @@ import Recipe from "./Recipe";
 
 export default {
   name: "HelloWorld",
-  data: function(){
+  data: function() {
     return {
       keywords: "",
       cocktails: recipes
-    }
+    };
   },
   computed: {
-    results: function(){
-      if(!this.keywords){
+    results: function() {
+      if (!this.keywords) {
         return this.cocktails;
       }
-      const keywordList = this.keywords.split(',').map(k=>k.trim().toLowerCase());
+      const keywordList = this.keywords
+        .split(",")
+        .map(k => k.trim().toLowerCase());
       return this.cocktails.filter(c => {
-        const matchesIngredient = keywordList.every(k => ~c.ingredients.findIndex(i => ~i.detail.name.toLowerCase().indexOf(k) || (i.detail.description && ~i.detail.description.toLowerCase().indexOf(k))));
-        return matchesIngredient || keywordList.every(k => c.name.toLowerCase().includes(k));
-      })
+        const matchesIngredient = keywordList.every(k =>
+          c.ingredients.some(
+            i =>
+              (i.detail.name || "").toLowerCase().includes(k) ||
+              (i.detail.description || "").toLowerCase().includes(k)
+          )
+        );
+        const matchesName = keywordList.every(k =>
+          c.name.toLowerCase().includes(k)
+        );
+        const matchesPrep = keywordList.every(k =>
+          (c.prep || "").toLowerCase().includes(k)
+        );
+        return matchesIngredient || matchesName || matchesPrep;
+      });
     }
   },
   components: {
